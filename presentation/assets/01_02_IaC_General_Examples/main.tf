a4a3d1c7bb91ac5f5ff6f3971eb672888a5e76e5
dha <Daniel.Hasselwander@ti8m.ch>
2023-11-29 02:53:08 +0100

    add assets

---
+++ presentation/assets/01_02_IaC_General_Examples/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.82.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = "switzerlandnorth"
+  name     = "rg_test"
+}
+
+resource "azurerm_storage_account" "storagaeaccount" {
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  location                 = "switzerlandnorth"
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_storage_container" "stcontainer" {
+  name                  = "container"
+  storage_account_name  = azurerm_storage_account.storagaeaccount.name
+  container_access_type = "blob"
+}
